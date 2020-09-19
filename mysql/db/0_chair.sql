90b641d6183ccf79f6e8ea3873b2790007631838
inductor <kohei.ota@hpe.com>
2020-09-20 02:36:32 +0900

    commit

---
+++ mysql/db/0_chair.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS isuumo;
+CREATE DATABASE isuumo;
+
+DROP TABLE IF EXISTS isuumo.chair;
+
+CREATE TABLE isuumo.chair
+(
+    id          INTEGER         NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)     NOT NULL,
+    description VARCHAR(4096)   NOT NULL,
+    thumbnail   VARCHAR(128)    NOT NULL,
+    price       INTEGER         NOT NULL,
+    height      INTEGER         NOT NULL,
+    width       INTEGER         NOT NULL,
+    depth       INTEGER         NOT NULL,
+    color       VARCHAR(64)     NOT NULL,
+    features    VARCHAR(64)     NOT NULL,
+    kind        VARCHAR(64)     NOT NULL,
+    popularity  INTEGER         NOT NULL,
+    stock       INTEGER         NOT NULL
+);
+
+create index chair_idx on isuumo.chair (price);
---
+++ mysql/db/0_estate.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS isuumo;
+CREATE DATABASE isuumo;
+
+DROP TABLE IF EXISTS isuumo.estate;
+
+CREATE TABLE isuumo.estate
+(
+    id          INTEGER             NOT NULL PRIMARY KEY,
+    name        VARCHAR(64)         NOT NULL,
+    description VARCHAR(4096)       NOT NULL,
+    thumbnail   VARCHAR(128)        NOT NULL,
+    address     VARCHAR(128)        NOT NULL,
+    latitude    DOUBLE PRECISION    NOT NULL,
+    longitude   DOUBLE PRECISION    NOT NULL,
+    rent        INTEGER             NOT NULL,
+    door_height INTEGER             NOT NULL,
+    door_width  INTEGER             NOT NULL,
+    features    VARCHAR(64)         NOT NULL,
+    popularity  INTEGER             NOT NULL
+);
+
+create index estate_idx on isuumo.estate (rent);
